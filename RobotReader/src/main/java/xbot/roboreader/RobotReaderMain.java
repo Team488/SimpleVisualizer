11dd961ccf9e0558f29393577585aa539a5255d5
John Gilbert <johngilb@gmail.com>
2018-09-29 13:22:51 -0700

    proof of concept for reading network table values

--- RobotReader/src/main/java/xbot/roboreader/RobotReaderMain.java
+++ RobotReader/src/main/java/xbot/roboreader/RobotReaderMain.java
@@ -1,9 +1,30 @@
 package xbot.roboreader;
 
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
+
 public class RobotReaderMain {
 
-    public static void main(String args[])
+    public static void main(String args[]) throws InterruptedException
     {
         System.out.println("quick gradle test");
+
+        NetworkTableInstance inst = NetworkTableInstance.getDefault();
+
+        NetworkTable table = inst.getTable("Testing");
+        NetworkTableEntry entry = table.getEntry("testo");
+        NetworkTableEntry entry2 = table.getEntry("secondTesto");
+
+        inst.startClient("localhost");
+
+        while(true) {
+            Thread.sleep(1000);
+
+            entry2.setBoolean(true);
+
+            System.out.println(entry.getString("no value yet"));
+        }
+
     }
 }
