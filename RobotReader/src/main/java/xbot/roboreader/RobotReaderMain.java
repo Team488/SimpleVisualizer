03af29910465b84c97297388ae9a4308a6df97ec
John Gilbert <johngilb@gmail.com>
2018-10-13 13:38:30 -0700

    Finish e2e_prototype

--- RobotReader/src/main/java/xbot/roboreader/RobotReaderMain.java
+++ RobotReader/src/main/java/xbot/roboreader/RobotReaderMain.java
@@ -1,6 +1,14 @@
 package xbot.roboreader;
 
+import java.util.concurrent.TimeUnit;
+
+import org.influxdb.BatchOptions;
+import org.influxdb.InfluxDB;
 import org.influxdb.InfluxDBFactory;
+import org.influxdb.dto.Point;
+import org.influxdb.dto.Pong;
+import org.influxdb.dto.Query;
+import org.influxdb.dto.QueryResult;
 
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
@@ -14,21 +22,81 @@ public class RobotReaderMain {
 
         NetworkTableInstance inst = NetworkTableInstance.getDefault();
 
-        NetworkTable table = inst.getTable("Testing");
-        NetworkTableEntry entry = table.getEntry("testo");
-        NetworkTableEntry entry2 = table.getEntry("secondTesto");
+        NetworkTable table = inst.getTable("SmartDashboard/PoseSubsystem");
+        NetworkTableEntry netX = table.getEntry("Total distance X");
+        NetworkTableEntry netY = table.getEntry("Total distance Y");
+        NetworkTableEntry netHeading = table.getEntry("CurrentHeading");
+        
 
-        inst.startClient("localhost");
+        String networkTableIpAddress = "10.4.88.2";
+        System.out.println("Beginning connection phase");
+        inst.startClient(networkTableIpAddress);
 
-        while(true) {
+        InfluxDB idb = InfluxDBFactory.connect("http://localhost:8086", "root", "root");
+        Pong p = idb.ping();
+
+        String poseDbName = "RobotPose";
+        String poseDbRetentionPolicy = "RobotPoseRetentionPolicy";
+
+        idb.createDatabase(poseDbName);
+        idb.setDatabase(poseDbName);
+        idb.createRetentionPolicy(poseDbRetentionPolicy, poseDbName, "30d", "30m", 2, true);
+
+        idb.enableBatch();
+        
+        for (int i = 0; i < 60; i++) {
             Thread.sleep(1000);
 
-            entry2.setBoolean(true);
+            System.out.println(netX.getDouble(0));
+            System.out.println(netY.getDouble(0));
+            System.out.println(netHeading.getDouble(0));
 
-            System.out.println(entry.getString("no value yet"));
+            idb.write(poseDbName, poseDbRetentionPolicy, Point.measurement("Pose")
+            .time(System.currentTimeMillis(), TimeUnit.MILLISECONDS)
+            .addField("X", netX.getDouble(0))
+            .addField("Y", netY.getDouble(0))
+            .addField("Heading", netHeading.getDouble(0))
+            .build());
+
+            idb.flush();
         }
 
-        //
+        Query query = new Query("SELECT * FROM \"Pose\"", poseDbName);
+        QueryResult qr = idb.query(query);
+        System.out.println(qr.getResults().toString());
+/*
+        InfluxDB idb = InfluxDBFactory.connect("http://localhost:8086", "root", "root");
+        Pong p = idb.ping();
 
+        String poseDbName = "RobotPose";
+        String poseDbRetentionPolicy = "RobotPoseRetentionPolicy";
+
+        idb.createDatabase(poseDbName);
+        idb.setDatabase(poseDbName);
+        idb.createRetentionPolicy(poseDbRetentionPolicy, poseDbName, "30d", "30m", 2, true);
+
+        System.out.println(idb.databaseExists(poseDbName));
+        
+        idb.enableBatch();
+        idb.write(poseDbName, poseDbRetentionPolicy, Point.measurement("Pose")
+            .time(System.currentTimeMillis(), TimeUnit.MILLISECONDS)
+            .addField("X", 13.0)
+            .addField("Y", 10.0)
+            .addField("Heading", 177.0)
+            .build());
+
+        idb.flush();
+
+        Query query = new Query("SHOW MEASUREMENTS", poseDbName);
+        QueryResult qr = idb.query(query);
+        System.out.println(qr.getResults().toString());
+        
+        query = new Query("SELECT * FROM \"Pose\"", poseDbName);
+        qr = idb.query(query);
+        System.out.println(qr.getResults().toString());
+
+        System.out.println(p.getResponseTime());
+
+        */
     }
 }
