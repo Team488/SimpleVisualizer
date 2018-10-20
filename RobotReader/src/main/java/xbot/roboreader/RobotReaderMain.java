00ac0a02b9ecfbb7dc928ca370eeac545cf8a4df
John Gilbert <johngilb@gmail.com>
2018-10-20 12:07:53 -0700

    Finish GetGradleBuilding

--- RobotReader/src/main/java/xbot/roboreader/RobotReaderMain.java
+++ RobotReader/src/main/java/xbot/roboreader/RobotReaderMain.java
@@ -1,8 +1,8 @@
 package xbot.roboreader;
 
+import java.util.concurrent.Callable;
 import java.util.concurrent.TimeUnit;
 
-import org.influxdb.BatchOptions;
 import org.influxdb.InfluxDB;
 import org.influxdb.InfluxDBFactory;
 import org.influxdb.dto.Point;
@@ -13,30 +13,61 @@ import org.influxdb.dto.QueryResult;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
 
-public class RobotReaderMain {
+@Command(description = "Reads telemetry from the robot and passes it into a local influxDb table.")
+public class RobotReaderMain implements Callable<Void>{
+
+    @Option(names = {"-networkTableServerAddress", "-n"}, description = {"localhost", "10.4.88.2"}, required = false)
+    private String networkTableServerAddress = "10.4.88.2";
+
+    @Option(names = {"-influxDbServerAddress", "-i"}, description = {"localhost", "127.0.0.1"}, required = false)
+    private String influxDbServerAddress = "localhost";
+
+    @Option(names = {"-refreshRateMs", "-r"}, description = {"50", "100", "1000"}, required = false)
+    private int refreshRateMs = 1000;
+
+    @Option(names = {"-debug", "-d"}, description = {"Enable debug logging"}, required = false)
+    private boolean debugLogging;
+
 
     public static void main(String args[]) throws InterruptedException
     {
-        System.out.println("quick gradle test");
+        CommandLine.call(new RobotReaderMain(), args);
+    }
+
+    public Void call() throws Exception
+    {
+        String fullInfluxAddress = "http://" + influxDbServerAddress  + ":8086";
+
+        System.out.println("NetworkTable Address: " + networkTableServerAddress);
+        System.out.println("InfluxDB Address: " + fullInfluxAddress);
 
         NetworkTableInstance inst = NetworkTableInstance.getDefault();
-
         NetworkTable table = inst.getTable("SmartDashboard/PoseSubsystem");
         NetworkTableEntry netX = table.getEntry("Total distance X");
         NetworkTableEntry netY = table.getEntry("Total distance Y");
         NetworkTableEntry netHeading = table.getEntry("CurrentHeading");
         
 
-        String networkTableIpAddress = "10.4.88.2";
-        System.out.println("Beginning connection phase");
-        inst.startClient(networkTableIpAddress);
+        
+        System.out.println("Starting a network table client...");
+        inst.startClient(networkTableServerAddress);
+        Thread.sleep(2000);
+        System.out.println("Connection status: " +inst.isConnected());
 
-        InfluxDB idb = InfluxDBFactory.connect("http://localhost:8086", "root", "root");
+        System.out.println("Starting influxDb client...");
+        InfluxDB idb = InfluxDBFactory.connect(fullInfluxAddress, "root", "root");
         Pong p = idb.ping();
+        System.out.println("influxDb Ping results: " + p.getResponseTime() + "ms");
 
         String poseDbName = "RobotPose";
         String poseDbRetentionPolicy = "RobotPoseRetentionPolicy";
+        String poseDbMeasurement = "Pose";
+        System.out.println("Creating/connecting to influxDb database with name: " + poseDbName);
+        System.out.println("Writing measurement with name: " + poseDbMeasurement);
 
         idb.createDatabase(poseDbName);
         idb.setDatabase(poseDbName);
@@ -47,11 +78,13 @@ public class RobotReaderMain {
         for (int i = 0; i < 60; i++) {
             Thread.sleep(1000);
 
-            System.out.println(netX.getDouble(0));
-            System.out.println(netY.getDouble(0));
-            System.out.println(netHeading.getDouble(0));
+            if (debugLogging) {
+                System.out.println(netX.getDouble(0));
+                System.out.println(netY.getDouble(0));
+                System.out.println(netHeading.getDouble(0));
+            }
 
-            idb.write(poseDbName, poseDbRetentionPolicy, Point.measurement("Pose")
+            idb.write(poseDbName, poseDbRetentionPolicy, Point.measurement(poseDbMeasurement)
             .time(System.currentTimeMillis(), TimeUnit.MILLISECONDS)
             .addField("X", netX.getDouble(0))
             .addField("Y", netY.getDouble(0))
@@ -61,42 +94,6 @@ public class RobotReaderMain {
             idb.flush();
         }
 
-        Query query = new Query("SELECT * FROM \"Pose\"", poseDbName);
-        QueryResult qr = idb.query(query);
-        System.out.println(qr.getResults().toString());
-/*
-        InfluxDB idb = InfluxDBFactory.connect("http://localhost:8086", "root", "root");
-        Pong p = idb.ping();
-
-        String poseDbName = "RobotPose";
-        String poseDbRetentionPolicy = "RobotPoseRetentionPolicy";
-
-        idb.createDatabase(poseDbName);
-        idb.setDatabase(poseDbName);
-        idb.createRetentionPolicy(poseDbRetentionPolicy, poseDbName, "30d", "30m", 2, true);
-
-        System.out.println(idb.databaseExists(poseDbName));
-        
-        idb.enableBatch();
-        idb.write(poseDbName, poseDbRetentionPolicy, Point.measurement("Pose")
-            .time(System.currentTimeMillis(), TimeUnit.MILLISECONDS)
-            .addField("X", 13.0)
-            .addField("Y", 10.0)
-            .addField("Heading", 177.0)
-            .build());
-
-        idb.flush();
-
-        Query query = new Query("SHOW MEASUREMENTS", poseDbName);
-        QueryResult qr = idb.query(query);
-        System.out.println(qr.getResults().toString());
-        
-        query = new Query("SELECT * FROM \"Pose\"", poseDbName);
-        qr = idb.query(query);
-        System.out.println(qr.getResults().toString());
-
-        System.out.println(p.getResponseTime());
-
-        */
+        return null;
     }
 }
